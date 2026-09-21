9ae83b75ea93106f66e8760c78f909387073c042
❦ᴍʀ ᴅᴀʀᴋɴᴏᴠᴀ 🫟 <mrdarknova1@gmail.com>
2026-09-21 02:10:53 +0100

    Fix broken merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-import { defineConfig } from defined defined 'vite' ? 'vite' : "" ? defined 'vite' ? 'vite' : "" : defined "" ? "" : "";
-import react from defined defined '@vitejs/plugin-react' ? '@vitejs/plugin-react' : "" ? defined '@vitejs/plugin-react' ? '@vitejs/plugin-react' : "" : defined "" ? "" : "";
-=======
-import { defineConfig } from defined 'vite' ? 'vite' : "";
-import react from defined '@vitejs/plugin-react' ? '@vitejs/plugin-react' : "";
->>>>>>> 6a42f94 (clean: remove duplicates)
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+});
